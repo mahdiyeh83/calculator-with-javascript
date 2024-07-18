c85924f518745c4ef36526a504fe3732b1541816
mahdiyeh aghababayi <149269006+mahdiyeh83@users.noreply.github.com>
2024-07-18 17:54:04 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,52 +1,94 @@
-let currentDisplay = "";
-// let clearOnNextNum = false;
 
+let currentDisplay = "";
+let handelno = false;
+
+// getting number
 function formatNumber(number) {
   return new Intl.NumberFormat().format(number);
 }
 
+// function multiResult(result) {
+//   return result.replaceAll(/\*/g, '×');
+// }
+
+let display = document.getElementById("texteara");
+const arr = Array.from(buttons);
+arr.forEach((item) => {
+  item.addEventListener("click", ding);
+});
+
+// operators
 function ins(val) {
-  const display = document.getElementById("texteara");
   const operators = ["+", "-", "*", "/"];
   const lastChar = display.value.slice(-1);
 
-  if (operators.includes(val) && operators.includes(lastChar)) {
-    return;
+  if (
+    display.style.background == "mediumSpringGreen" ||
+    display.style.background == "rgb(255, 30, 0)"
+  ) {
+    currentDisplay = val;
+    display.value = currentDisplay;
+    display.style.background = "white";
+  } else {
+    if (handelno == true) {
+      clearDisplay();
+      handelno = false;
+    } else {
+      // zero at the beginning
+      if (val === "0" && display.value === "") {
+        return;
+      }
+
+      // entering '*' or '/' at the beginning
+      if ((val === "*" || val === "/") && display.value === "") {
+        return;
+      }
+
+      // Prevent entering consecutive operators
+      if (operators.includes(val) && operators.includes(lastChar)) {
+        return;
+      }
+
+      // leading zero when entering a nonzero number
+      if (display.value === "0" && !operators.includes(val)) {
+        currentDisplay = val;
+        display.value = currentDisplay;
+      } else {
+        currentDisplay += val;
+        display.value = currentDisplay;
+      }
+    }
   }
-  currentDisplay += val;
-  display.value = currentDisplay;
 }
 
+// clear all
 function clearDisplay() {
-  const display = document.getElementById("texteara");
   currentDisplay = "";
   display.value = currentDisplay;
   display.style.background = "white";
 }
-// کار نمیکنه
-// function handleNumber(number) {
-//   if (clearOnNextNum) {
-//     clearOnNextNum = false;
-//     currentDisplay = "";
-//     display.style.background = "white";
-//   }
-//   currentDisplay += number;
-//   display.value = currentDisplay;
-// }
 
+// calculate nums
 function calculate() {
-  const display = document.getElementById("texteara");
   try {
+    handelno = true;
     const result = eval(currentDisplay);
-    display.value = formatNumber(result);
-    currentDisplay = result.toString();
-    display.style.background = "MediumSpringGreen";
 
-    const inpValue = display.value.replaceAll(",", "");
-    let calculateValue = eval(inpValue);
-    calculateValue += "";
-    calculateValue = new Intl.NumberFormat().format(calculateValue);
-    display.value = calculateValue;
+    // Check for division by zero
+    if (!isFinite(result)) {
+      display.value = "∞";
+      currentDisplay = "";
+      display.style.background = "mediumSpringGreen";
+    } else {
+      display.value = formatNumber(result);
+      currentDisplay = result.toString();
+      display.style.background = "mediumSpringGreen";
+      const inpValue = display.value.replaceAll(",", "");
+      let calculateValue = eval(inpValue);
+      calculateValue += "";
+      calculateValue = new Intl.NumberFormat().format(calculateValue);
+      display.value = calculateValue;
+    }
   } catch (error) {
     display.value = "Error";
     display.style.background = "rgb(255, 30, 0)";
@@ -54,12 +96,20 @@ function calculate() {
   }
 }
 
+// deleting one by one
+function singleChar() {
+  display.value = display.value.slice(0, display.value.length - 1);
+  if (!display.value) {
+    display.style.background = "white";
+    clearDisplay();
+  }
+}
+
+// button sound
 function ding() {
   var sound = new Audio(
     "https://proxy.notificationsounds.com/message-tones/out-of-nowhere-message-tone/download/file-sounds-1231-out-of-nowhere.mp3"
   );
   sound.play();
 }
-const display = document
-  .getElementsByTagName("button")[0]
-  .addEventListener("click", ding);
+
